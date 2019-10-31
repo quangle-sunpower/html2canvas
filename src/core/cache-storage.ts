7c20634f23f4c58c99ae6f54497e95a1ef04b524
quangle-sunpower <51136317+quangle-sunpower@users.noreply.github.com>
2019-10-31 15:18:34 -0500

    [b] add `ts` timestamp query param to image URL to fix CORs issue with iOS Safari

--- src/core/cache-storage.ts
+++ src/core/cache-storage.ts
@@ -127,7 +127,10 @@ export class Cache {
             //ios safari 10.3 taints canvas with data urls unless crossOrigin is set to anonymous
             if (isInlineBase64Image(src) || useCORS) {
                 img.crossOrigin = 'anonymous';
+                const timestamp = new Date().getTime();
+                img.src = img.src.includes('?') ? `${img.src}&ts=${timestamp}` : `${img.src}?ts=${timestamp}`;
             }
+            
             img.src = src;
             if (img.complete === true) {
                 // Inline XML images may fail to parse, throwing an Error later on
